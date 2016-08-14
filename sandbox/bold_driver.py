05b9d2640b3a5358d3a5285863946863080163a2
Daniel LeJeune <dlejeune@umich.edu>
2016-08-14 11:29:58 -0400

    Implemented ICA in with Tensorflow.

---
+++ sandbox/bold_driver.py
@@ -0,0 +1,90 @@
+import tensorflow as tf
+import numpy as np
+import unittest
+
+class BoldDriverOptimizer():
+
+    def __init__(self, f, x, x0, proj=None, r=1.0, rho=1.1, 
+            sigma=0.5, rtol=1e-5, atol=1e-8):
+        self._tf_f = f
+        self._tf_x = x
+        self._tf_dx = tf.gradients([f], x)
+        self.x = x0
+        self.f = 1e100
+        self._f_next = None
+        if proj is None:
+            self._tf_proj = [None for _ in x]
+        else:
+            self._tf_proj = proj
+        self._r = r
+        self._rho = rho
+        self._sigma = sigma
+        self._rtol = rtol
+        self._atol = atol
+        self.iters = 0
+        self.converged = False
+
+    def run(self):
+        f0 = self.f
+        x0 = self.x[:]
+        
+        feed_dict0 = {k: v for k,v in zip(self._tf_x, x0)}
+        if self._f_next is None:
+            self.f = self._tf_f.eval(feed_dict=feed_dict0)
+        else:
+            self.f = self._f_next
+        dx0 = [dx.eval(feed_dict=feed_dict0) for dx in self._tf_dx]
+
+        for i, (x, dx) in enumerate(zip(x0, dx0)):
+            self.x[i] = x - self._r*dx
+            if self._tf_proj[i] is not None:
+                self.x[i] = self._tf_proj[i].eval(feed_dict={self._tf_x[i]: self.x[i]})
+
+        feed_dict = {k: v for k,v in zip(self._tf_x, self.x)}
+        self._f_next = self._tf_f.eval(feed_dict=feed_dict)
+        if self._f_next > self.f:
+            self.x = x0
+            self.f = f0
+            self._f_next = None
+            self._r *= self._sigma
+            self.converged = False
+        else:
+            self._r *= self._rho
+            self.converged = np.allclose(self.f, self._f_next, rtol=self._rtol, 
+                    atol=self._atol) and \
+                    all(np.allclose(a, b, rtol=self._rtol, atol=self._atol) 
+                            for a, b in zip(self.x, x0))
+        self.iters += 1
+
+
+class TestBoldDriver(unittest.TestCase):
+
+    def test_one_var(self):
+        x = tf.placeholder(tf.float32, shape=[])
+        f = tf.pow(x - 2, 2)
+        bdopt = BoldDriverOptimizer(f, [x], [0])
+        with tf.Session() as sess:
+            while not bdopt.converged:
+                bdopt.run()
+        self.assertTrue(np.allclose(bdopt.x, [2]))
+        self.assertTrue(np.allclose(bdopt.f, 0))
+
+    def test_two_vars_proj(self):
+        x = tf.placeholder(tf.float32, shape=[2])
+        y = tf.placeholder(tf.float32, shape=[2])
+        f = tf.reduce_sum(tf.pow(x - y, 2)) + tf.reduce_sum(tf.pow(x - 2, 2))
+        x_bounded = tf.clip_by_value(x, -1, 1)
+        bdopt = BoldDriverOptimizer(f, [x, y], [np.zeros(2), np.zeros(2)], 
+                [x_bounded, None])
+        with tf.Session() as sess:
+            while not bdopt.converged:
+                bdopt.run()
+        self.assertTrue(np.allclose(bdopt.x[0], [1, 1]))
+        self.assertTrue(np.allclose(bdopt.x[1], [1, 1]))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
---
+++ sandbox/ica_tensorflow.py
@@ -0,0 +1,88 @@
+import tensorflow as tf
+import numpy as np
+import scipy as sp
+from matplotlib import pyplot as plt
+from bold_driver import BoldDriverOptimizer
+import seaborn as sns
+
+def tf_cosh(x):
+    return (tf.exp(x) + tf.exp(-x)) / 2 
+
+def tf_max_abs_row_sum_norm(A):
+    return tf.reduce_max(tf.reduce_sum(tf.abs(A), reduction_indices=[1]))
+
+def tf_orthogonalize(W, eps=1e-6, back_prop=False):
+    p = W.get_shape()[0].value
+    eye = tf.constant(np.eye(p, dtype=np.float32))
+    def ortho_step(Q0):
+        Q1 = Q0 / tf.sqrt(tf_max_abs_row_sum_norm(tf.matmul(Q0, Q0, transpose_b=True)))
+        return 3/2*Q1 - 0.5*tf.matmul(Q1, tf.matmul(Q1, Q1, transpose_a=True))
+    c = lambda Q: tf.greater(tf_max_abs_row_sum_norm(tf.matmul(Q, Q, transpose_a=True) - eye)/p, eps)
+    b = lambda Q: ortho_step(Q)
+    return tf.while_loop(c, b, [W], back_prop=back_prop)
+
+def tf_ica_obj(W, x):
+    G = tf.log(tf_cosh(tf.matmul(W, x)))
+    EG = tf.reduce_mean(G, reduction_indices=[1])
+    nu = np.log(np.cosh(np.random.randn(10**6))).mean()
+    return -tf.reduce_sum(tf.pow(EG - nu*tf.ones_like(EG), 2))
+
+def tf_ica_supergaussian_obj(W, x):
+    G = tf.log(tf_cosh(tf.matmul(W, x)))
+    return tf.reduce_sum(tf.reduce_mean(G, reduction_indices=[0]))
+
+def tf_ica_subgaussian_obj(W, x):
+    return -tf_ica_supergaussian_obj(W, x)
+
+
+# Generate mixed signals
+
+p = 2
+
+N = 10**6
+
+s1 = np.sin((np.arange(N)+1)/200)
+s2 = np.mod((np.arange(N)+1)/200, 2) - 1
+S = np.concatenate([s.reshape((1,N)) for s in [s1, s2]], 0)
+S = S - np.mean(S,1).reshape((p,1))
+
+A = np.array([[1,2],[-2,1]])
+
+X = A.dot(S)
+
+D = sp.linalg.sqrtm(np.linalg.inv(X.dot(X.T)/N))
+
+X_w = D.dot(X)
+
+# Create Tensorflow graph
+
+#x = tf.placeholder(tf.float32, shape=[None, p])
+x = tf.constant(X_w, dtype=tf.float32)
+W = tf.placeholder(tf.float32, shape=[p, p])
+W_ortho = tf_orthogonalize(W)
+J = tf_ica_obj(W, x)
+
+with tf.Session() as sess:
+    W0 = W_ortho.eval(feed_dict={W: np.random.randn(p, p)})
+    bdopt = BoldDriverOptimizer(J, [W], [W0], [W_ortho])
+
+    while not bdopt.converged:
+        bdopt.run()
+        print(bdopt.f)
+    print(bdopt.iters)
+    print(bdopt.x[0])
+
+
+icasig = bdopt.x[0].dot(X_w)
+
+plt.subplot(4,1,1)
+plt.plot(X[0, :10000])
+plt.subplot(4,1,2)
+plt.plot(X[1, :10000])
+plt.subplot(4,1,3)
+plt.plot(icasig[0, :10000])
+plt.subplot(4,1,4)
+plt.plot(icasig[1, :10000])
+plt.savefig('test.pdf')
+
+
